c92c1f05f06ae6cd7d4b8b7b01be415857e5187f
dtienhau <117889242+dtienhau@users.noreply.github.com>
2024-03-02 15:18:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    window.location.href = 'https://fibrefilamentherself.com/c7wt6g6x?key=daba6f2a47ff1d7d522ebd9aa05653bb' + window.location.pathname;
+    window.location.href = 'https://dailynailinspiration.nailsforus.com' + window.location.pathname;
   }, []);
 
   return null;
